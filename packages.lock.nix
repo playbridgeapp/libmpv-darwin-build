db67b7d1d2f7cd3afe2f8f2380994ec85279c416
Atul Mehla <atulmehlasmart@gmail.com>
2026-08-24 20:59:19 -0230

    build: bump mpv to 0.40.0 and ffmpeg to 7.1.1

--- packages.lock.nix
+++ packages.lock.nix
@@ -5,9 +5,9 @@
     sha256 = "a4003623cdc0109dec3aac8435520aa3fb12c4d69454fa227f2658cdb6dab5fa";
   };
   ffmpeg = {
-    version = "6.0";
-    url = "https://ffmpeg.org/releases/ffmpeg-6.0.tar.xz";
-    sha256 = "57be87c22d9b49c112b6d24bc67d42508660e6b718b3db89c44e47e289137082";
+    version = "7.1.1";
+    url = "https://ffmpeg.org/releases/ffmpeg-7.1.1.tar.xz";
+    sha256 = "733984395e0dbbe5c046abda2dc49a5544e7e0e1e2366bba849222ae9e3a03b1";
   };
   fftools-ffi = {
     version = "9b0d4da0";
@@ -75,9 +75,9 @@
     sha256 = "a420fcf7103e54e775c383e3751729b8fb2dcd087f6165befd13f28315f754f5";
   };
   mpv = {
-    version = "0.36.0";
-    url = "https://github.com/mpv-player/mpv/archive/refs/tags/v0.36.0.tar.gz";
-    sha256 = "29abc44f8ebee013bb2f9fe14d80b30db19b534c679056e4851ceadf5a5e8bf6";
+    version = "0.40.0";
+    url = "https://github.com/mpv-player/mpv/archive/refs/tags/v0.40.0.tar.gz";
+    sha256 = "10a0f4654f62140a6dd4d380dcf0bbdbdcf6e697556863dc499c296182f081a3";
   };
   uchardet = {
     version = "0.0.8";
