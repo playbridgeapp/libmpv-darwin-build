1613d7d55edd26cf49813a371e523c4c1fd33ec9
Atul Mehla <atulmehlasmart@gmail.com>
2026-08-24 22:28:35 -0230

    build: drop meson options removed in mpv 0.40

--- nix/packages/mk-pkg-mpv/default.nix
+++ nix/packages/mk-pkg-mpv/default.nix
@@ -84,7 +84,6 @@ pkgs.stdenvNoCC.mkDerivation {
       -Dlibmpv=false `# libmpv library`
       -Dbuild-date=false `# whether to include binary compile time`
       -Dtests=false `# unit tests (development only)`
-      -Dta-leak-report=false `# enable ta leak report by default (development only)`
 
       `# misc features`
       -Dcdda=disabled `# cdda support (libcdio)`
@@ -102,12 +101,10 @@ pkgs.stdenvNoCC.mkDerivation {
       -Drubberband=disabled `# librubberband support`
       -Dsdl2=disabled `# SDL2`
       -Dsdl2-gamepad=disabled `# SDL2 gamepad input`
-      -Dstdatomic=disabled `# C11 stdatomic.h`
       -Duchardet=disabled `# uchardet support`
       -Duwp=disabled `# Universal Windows Platform`
       -Dvapoursynth=disabled `# VapourSynth filter bridge`
       -Dvector=disabled `# GCC vector instructions`
-      -Dwin32-internal-pthreads=disabled `#internal pthread wrapper for win32 (Vista+)`
       -Dzimg=disabled `# libzimg support (high quality software scaler)`
       -Dzlib=disabled `# zlib`
 
@@ -146,8 +143,6 @@ pkgs.stdenvNoCC.mkDerivation {
       -Dgl-win32=disabled `# OpenGL Win32 Backend`
       -Dgl-x11=disabled `# OpenGL X11/GLX (deprecated/legacy)`
       -Djpeg=disabled `# JPEG support`
-      -Dlibplacebo=disabled `# libplacebo support`
-      -Drpi=disabled `# Raspberry Pi support`
       -Dsdl2-video=disabled `# SDL2 video output`
       -Dshaderc=disabled `# libshaderc SPIR-V compiler`
       -Dsixel=disabled `# Sixel`
@@ -159,7 +154,6 @@ pkgs.stdenvNoCC.mkDerivation {
       -Dvaapi-drm=disabled `# VAAPI (DRM/EGL support)`
       -Dvaapi-wayland=disabled `# VAAPI (Wayland support)`
       -Dvaapi-x11=disabled `# VAAPI (X11 support)`
-      -Dvaapi-x-egl=disabled `# VAAPI EGL on X11`
       -Dvulkan=disabled `# Vulkan context support`
       -Dwayland=disabled `# Wayland`
       -Dx11=disabled `# X11`
@@ -173,13 +167,9 @@ pkgs.stdenvNoCC.mkDerivation {
       -Dd3d9-hwaccel=disabled `# DXVA2 hwaccel`
       -Dgl-dxinterop-d3d9=disabled `# OpenGL/DirectX Interop Backend DXVA2 interop`
       -Dios-gl=disabled `# iOS OpenGL ES hardware decoding interop support`
-      -Drpi-mmal=disabled `# Raspberry Pi MMAL hwaccel`
       -Dvideotoolbox-gl=disabled `# Videotoolbox with OpenGL`
 
       `# macOS features`
-      -Dmacos-10-11-features=disabled `# macOS 10.11 SDK Features`
-      -Dmacos-10-12-2-features=disabled `# macOS 10.12.2 SDK Features`
-      -Dmacos-10-14-features=disabled `# macOS 10.14 SDK Features`
       -Dmacos-cocoa-cb=disabled `# macOS libmpv backend`
       -Dmacos-media-player=disabled `# macOS Media Player support`
       -Dmacos-touchbar=disabled `# macOS Touch Bar support`
